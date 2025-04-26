0140ab4841420797afed2529f5798487aa0d7a2e
mari-mbiru <marimbiru@gmail.com>
2025-04-26 13:15:42 +0300

    -init project

---
+++ src/main/java/com/example/excel_file_processor/ExcelFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.excel_file_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelFileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelFileProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/excel_file_processor/ExcelFileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excel_file_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelFileProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
