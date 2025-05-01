20cb4b6f5c37c802af8a8e302f29fc36afc59937
mari-mbiru <marimbiru@gmail.com>
2025-05-01 14:07:46 +0300

    -add exception handler for maximum size exceeded

--- src/main/java/com/example/excel_file_processor/exception/RestExceptionHandler.java
+++ src/main/java/com/example/excel_file_processor/exception/RestExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 @ControllerAdvice
@@ -24,11 +25,19 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(MissingServletRequestPartException.class)
-    public ResponseEntity<Object> handleMissingParams(MissingServletRequestPartException ex, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleMissingParams(MissingServletRequestPartException ex, HttpServletRequest request) {
         String name = ex.getRequestPartName();
         return new ResponseEntity<>(
                 new ErrorResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), "Parameter '" + name + "' is required", request.getRequestURI()),
                 HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> handleMaxSizeException(MaxUploadSizeExceededException ex, HttpServletRequest request) {
+        return new ResponseEntity<>(
+                new ErrorResponse(HttpStatus.PAYLOAD_TOO_LARGE.value(), HttpStatus.PAYLOAD_TOO_LARGE.getReasonPhrase(), "Request payload exceeded max size of 5MB", request.getRequestURI()),
+                HttpStatus.PAYLOAD_TOO_LARGE
         );
     }
 }
